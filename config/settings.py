1ae1525eedbb260cf69afdb8673b568b06c2668d
josuecede <jcedeno2417@utm.edu.ec>
2026-07-27 11:53:36 -0400

    Fix staticfiles storage to avoid missing sourcemap errors

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 
 STORAGES = {
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.StaticFilesStorage',
     },
 }
 
